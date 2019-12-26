2ad8edf24b7368fecacfad2afe91d7e2c3f5e2af
Ericcentrik <enaiman1@gmail.com>
2019-12-26 13:07:09 -0500

    created another class that renders the dynamic html

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,44 +12,58 @@ class Product {
     }
 }
 
-const productList = {
-    products: [
-        new Product(
-            'A pillow',
-            'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/201936/4102/img80c.jpg',
-            'A soft pillow',
-            19.99
-            ),
-
-        new Product(
-            'A carpet',
-            'https://mobileimages.lowes.com/product/converted/840712/840712131483.jpg?size=xl',
-            'A carpet which you might like - or not',
-            89.99
-        )
-    ],
-    render() {
-        const renderHook = document.getElementById('app');
-        const prodList = document.createElement('ul');
-        prodList.className = "product-list"
-        for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = "product-item";
-            prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageUrl}" alt="${prod.title}">
-                    <div class="product-item__content>
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button> Add to Cart</button>
-                    </div>
-                </div>
-                `;
-            prodList.append(prodEl)
-        }
-        renderHook.append(prodList)
+class ProductItem{
+    constructor(product){
+        this.product = product;
     }
 
+    render(){
+        const prodEl = document.createElement('li');
+        prodEl.className = "product-item";
+        prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                <div>
+                 <h2>${this.product.title}</h2>
+                 <h3>\$${this.product.price}</h3>
+                 <p>${this.product.description}</p>
+                 <button> Add to Cart</button>
+                </div>
+            </div>
+            `;
+            return prodEl
+    }
+}
+
+
+class ProductList{
+    products = [new Product(
+        'A pillow',
+        'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/201936/4102/img80c.jpg',
+        'A soft pillow',
+        19.99
+        ),
+
+    new Product(
+        'A carpet',
+        'https://mobileimages.lowes.com/product/converted/840712/840712131483.jpg?size=xl',
+        'A carpet which you might like - or not',
+        89.99
+    )];
+
+    constructor(){}
+
+    render(){
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = "product-list"
+    for (const prod of this.products) {
+       const productItem = new ProductItem(prod)
+       const prodEl = productItem.render();
+        prodList.append(prodEl)
+    }
+    renderHook.append(prodList)
+}
 };
+const productList = new ProductList();
 productList.render();
