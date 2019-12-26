90c3a4e5ba355e4ecca4fce81f4fecded122a1f1
Ericcentrik <enaiman1@gmail.com>
2019-12-26 12:37:57 -0500

    added a class to refactor my productList object

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,30 +1,43 @@
+class Product {
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
 
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
 
 const productList = {
-   products: [
-        {
-            title: 'A pillow', 
-            imgurl:'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/201936/4102/img80c.jpg', 
-            price: 19.99,
-             description: 'A soft pillow'
-        },
-        {
-            title: 'A carpet', 
-            imgurl:'https://mobileimages.lowes.com/product/converted/840712/840712131483.jpg?size=xl',
-            price: 89.99,
-             description: 'A carpet which you might like - or not'
-        }
-        ],
-        render() {
-            const renderHook = document.getElementById('app');
-            const prodList = document.createElement('ul');
-            prodList.className = "product-list"
-            for(const prod of this.products){
-                const prodEl = document.createElement('li');
-                prodEl.className ="product-item";
-                prodEl.innerHTML= `
+    products: [
+        new Product(
+            'A pillow',
+            'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/201936/4102/img80c.jpg',
+            'A soft pillow',
+            19.99
+            ),
+
+        new Product(
+            'A carpet',
+            'https://mobileimages.lowes.com/product/converted/840712/840712131483.jpg?size=xl',
+            'A carpet which you might like - or not',
+            89.99
+        )
+    ],
+    render() {
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = "product-list"
+        for (const prod of this.products) {
+            const prodEl = document.createElement('li');
+            prodEl.className = "product-item";
+            prodEl.innerHTML = `
                 <div>
-                    <img src="${prod.imgUrl}" alt="${prod.title}">
+                    <img src="${prod.imageUrl}" alt="${prod.title}">
                     <div class="product-item__content>
                         <h2>${prod.title}</h2>
                         <h3>\$${prod.price}</h3>
@@ -33,9 +46,10 @@ const productList = {
                     </div>
                 </div>
                 `;
-                prodEl.append(prodEl)
-            }
-            renderHook.append(prodList)
+            prodList.append(prodEl)
         }
+        renderHook.append(prodList)
+    }
 
-}
+};
+productList.render();
